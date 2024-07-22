1aa264920a5b30cd1ce5e1bc2545e5ac05cf3aff
NatalyKrotchenkoKPI <nataly.krotchenko@gmail.com>
2024-07-22 18:34:48 +0300

    Fifth commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'mob25h',
   e2e: {
     baseUrl: 'https://conduit.realworld.how',
-    specPattern: 'cypress/integration/tests/**/*.spec.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/integration/tests/**/*.spec.js',
+    setupNodeEvents(on, config) {
+      // Додаткові налаштування для event'ів, якщо потрібно
+    },
   },
+  env: {
+    email: 'dd29042004@gmail.com',
+    password: 'TestPassword'
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    html: true,
+    json: true
+  }
 });
