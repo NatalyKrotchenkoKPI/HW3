b839fd95fb2534190455b7fe98e183c62ae44aa3
NatalyKrotchenkoKPI <nataly.krotchenko@gmail.com>
2024-07-22 19:03:51 +0300

    Eighth commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://conduit.realworld.how',
-    specPattern: 'cypress/integration/tests/**/*.spec.js',
-    setupNodeEvents(on, config) {
-      // Додаткові налаштування для event'ів, якщо потрібно
-    },
+    e2e: {
+      baseUrl: "https://conduit.realworld.how",
+      projectId: 'mob25h',
+      specPattern: 'cypress/integration/tests/**/*.spec.js',
+      env: {
+        email: "dd29042004@gmail.com",
+        password: "TestPassword"
+      },
   },
   env: {
     email: 'dd29042004@gmail.com',
